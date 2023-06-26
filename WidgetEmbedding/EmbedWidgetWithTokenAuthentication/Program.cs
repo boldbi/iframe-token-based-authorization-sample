d608803cbb1aa88b435343b5f6c12323b2a92f93
Nithya <nithya.gopal@syncfusion.com>
2023-06-26 17:01:45 +0530

    dashboard to widget name changed

--- WidgetEmbedding/EmbedWidgetWithTokenAuthentication/Program.cs
+++ WidgetEmbedding/EmbedWidgetWithTokenAuthentication/Program.cs
@@ -14,15 +14,15 @@ namespace EmbedWidgetWithTokenAuthentication
         static void Main(string[] args)
         {
             // Variable declaration to form the embed URL 
-            var dashboardUrl = "https://onpremise-demo.boldbi.com/bi/site/site2/dashboards/9b5e9121-1379-4724-afba-c9674eb4f867/Education/adventure%20datasource%20dashboard?isWidgetMode=true&WidgetId=32ed09f7-49ef-4468-9c56-ccc376dbca8a&"; // URL of the dashboard to be embedded. Add '? or &' at the end of the URL based on your dashboard URL.
+            var dashboardUrl = "https://onpremise-demo.boldbi.com/bi/site/site2/dashboards/9b5e9121-1379-4724-afba-c9674eb4f867/Education/adventure%20datasource%20dashboard?isWidgetMode=true&WidgetId=32ed09f7-49ef-4468-9c56-ccc376dbca8a&"; // URL of the widget to be embedded. Add '? or &' at the end of the URL based on your widget URL.
 
-            // Mandatory Parameters to embed the dashboard with token based authentication.
+            // Mandatory Parameters to embed the widget with token based authentication.
             var nonce = Guid.NewGuid().ToString(); // To generate random GUID string.
             var userEmail = "demo@admin.com"; // User email id.
             double timeStamp = Math.Round((DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc)).TotalSeconds); // Current time as UNIX time stamp.
             string embedSecretKey = "pUu8DKaujKIyXdYAPWgvj1jW2OsCXuq";  // Secret key generated in the Bold BI application.
 
-            // Optional dashboard embed parameter
+            // Optional widget embed parameter
             bool canSaveView = true; // To show or hide the Save and SaveAs icon in the filter overview.
             bool hasViews = true; // To show or hide the views icon from the toolbar.
             bool hasExport = true; // To show or hide the export icon from the toolbar.
@@ -40,7 +40,7 @@ namespace EmbedWidgetWithTokenAuthentication
             var filePath = System.AppDomain.CurrentDomain.BaseDirectory + "embed.html"; // file named embed will be created in the extracted location in which the embed URL will be maintained.
             File.WriteAllText(filePath, iframe);
             string url = filePath;
-            Process.Start("chrome.exe", url); // By default, the embed dashboard will be rendered in Google Chrome. Can update the browser as you wish.
+            Process.Start("chrome.exe", url); // By default, the embed widget will be rendered in Google Chrome. Can update the browser as you wish.
         }
 
         // This method generates the hashed signature using the HMACSHA256 algorithm, which should be appended to the existing iframe URL as a query parameter named ‘embed_signature’.
